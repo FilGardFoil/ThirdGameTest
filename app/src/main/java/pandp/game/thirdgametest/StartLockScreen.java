bc9dcbcca1d3b71c435356870a697aea09fef1d6
FilGardFoil <a.ponikarovskii@gmail.com>
2015-07-07 11:06:29 +0300

    just test with lockscreen

---
+++ app/src/main/java/pandp/game/thirdgametest/StartLockScreen.java
@@ -0,0 +1,16 @@
+package pandp.game.thirdgametest;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class StartLockScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        startService(new Intent(this, MyService.class));
+        finish();
+    }
+}
